ba32ea2353f2f406a8502f79fee5e712738f44ad
MarNicGit <47538428+MarNicGit@users.noreply.github.com>
2021-07-10 00:03:47 +0200

    Add project files.

---
+++ Logalot/Logspammer.cs
@@ -0,0 +1,81 @@
+﻿using Bogus;
+using log4net;
+using System;
+
+namespace Logalot
+{
+    internal class Logspammer
+    {
+        private static readonly ILog Log4NetLog = log4net.LogManager.GetLogger(typeof(Program));
+
+        internal static void StartSpamming()
+        {
+            do
+            {
+                while(!Console.KeyAvailable)
+                {
+                    Spam();
+                    System.Threading.Thread.Sleep(new Random().Next(100, 2000));
+                }                
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Escape key pressed! Quitting!");
+            Console.ResetColor();
+        }
+
+        private static void Spam()
+        {            
+            string message = new Faker().Lorem.Sentence(3, 8);
+
+            var values = Enum.GetValues(typeof(LogLevel));
+            var randomLevel = (LogLevel)values.GetValue(new Random().Next(values.Length));
+            switch (randomLevel)
+            {
+                case LogLevel.Info: LogInfo(message);break;
+                case LogLevel.Warning: LogWarning(message);break;
+                case LogLevel.Error: LogError(message);break;
+                case LogLevel.Debug: LogDebug(message);break;
+                case LogLevel.Fatal: LogFatal(message);break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine(message);
+        }
+
+        private static void LogFatal(string message)
+        {
+            Log4NetLog.Fatal(message);
+        }
+
+        private static void LogDebug(string message)
+        {
+            Log4NetLog.Debug(message);
+        }
+
+        private static void LogError(string message)
+        {
+            Log4NetLog.Error(message);
+        }
+
+        private static void LogWarning(string message)
+        {
+            Log4NetLog.Warn(message);
+        }
+
+        private static void LogInfo(string message)
+        {
+            Log4NetLog.Info(message);
+        }
+    }
+
+    internal enum LogLevel
+    {
+        Info,
+        Warning,
+        Error,
+        Debug,
+        Fatal
+    }
+}
---
+++ Logalot/Program.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using System;
+
+namespace Logalot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setting up the loggers..");
+
+            InitializeLoggers();
+
+            Console.WriteLine("Time to spam some random logs!");
+
+            Logspammer spammer = new();
+
+            Logspammer.StartSpamming();
+        }
+        
+        private static void InitializeLoggers()
+        {
+            
+        }
+    }
+}
